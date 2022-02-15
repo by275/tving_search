a3f18efecaaa67ff21ec173ece9f6fb170d7b624
by275 <by275@protonmail.com>
2022-02-15 20:20:09 +0900

    0.3.4

--- plugin.py
+++ plugin.py
@@ -24,7 +24,7 @@ class PlugIn:
 
     plugin_info = {
         "category_name": "vod",
-        "version": "0.3.3",
+        "version": "0.3.4",
         "name": "tving_search",
         "home": "https://github.com/by275/tving_search",
         "more": "https://github.com/by275/tving_search",
